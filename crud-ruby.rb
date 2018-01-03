b76e145fa6ea481f1e92fa26a5e3e3550973a46e
isaac bustos <isaac.youthboard@futureindesign.org>
2018-01-03 15:06:56 -0700

    first commit

--- crud-ruby.rb
+++ crud-ruby.rb
@@ -4,8 +4,20 @@ require 'pry'
 
 # Constant - should not change
 BASE_URL = "http://json-server.devpointlabs.com/api/v1"
+response = HTTparty.get(BASE_URL)
+puts response.parsed_response
 
+class all_users
+    
+      BASE_URL 'json-server.devpointlabs.com'
+def initialize()
+    @options = { query:{ site service, page: page }}
+end
 # probably going to turn into an all_users method
+
+def questions
+    self.class.get("/2.2/Users", @options)
+end
 users = HTTParty.get("#{BASE_URL}/users").parsed_response
 
 users.each do |user|
